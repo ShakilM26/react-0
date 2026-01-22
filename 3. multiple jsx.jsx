e62f82d8cb7cc809dce955327fd0b738477d8f15
Shakil <shakil1emperor@gmail.com>
2026-01-22 21:56:01 +0600

    Add files via upload

---
+++ 3. multiple jsx.jsx
@@ -0,0 +1,12 @@
+function multiple_jsx(){
+  return (
+    <div> {/* parent element */}
+    {/* multiple JSX elements */}
+      <h1>It was a beautiful day!</h1>
+      <h2>In case I don't see ya,</h2>
+      <p>good afternoon, good evening, and good night!</p>
+    </div>
+  )
+}
+
+export default multiple_jsx
---
+++ 4. different component.jsx
@@ -0,0 +1,9 @@
+function Message(){
+  return <h2>It's Thursday</h2>
+}
+
+function App(){
+  return <Message/> {/* function call */}
+}
+
+export default App
