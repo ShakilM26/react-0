f59ba0759123b5f0ab2b8bff172a23be9a2081c9
Shakil <shakil1emperor@gmail.com>
2026-01-28 21:34:56 +0600

    Add files via upload

--- Quiz-App/pages/Playlists.jsx
+++ Quiz-App/pages/Playlists.jsx
@@ -1,8 +1,6 @@
 import playlists from '../data/playlists';
-import { useNavigate } from 'react-router-dom';
 
 function Playlists() {
-  const navigate = useNavigate();
 
   return (
     <div>
@@ -12,15 +10,9 @@ function Playlists() {
         style={{border: '1px solid #ccc',
           padding: '10px', marginBottom: '10px'
         }}>
-          <h3>{playlist.title}</h3>
-          <p>{playlist.description}</p>
-          <button onClick={() => navigate(`/quiz/${playlist.id}`)}>
-            Start Quiz
-          </button>
-          </div>
+        </div>
       ))}
     </div>
   );
 }
 
-export default Playlists;
