c6e88b917d3f3c81948846d23fdd2393276298bd
Shakil <shakil1emperor@gmail.com>
2026-01-28 22:22:24 +0600

    Update firebase.config.js

--- Quiz-App/firebase/firebase.config.js
+++ Quiz-App/firebase/firebase.config.js
@@ -1,14 +1,15 @@
 import {initializeApp} from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBX8NJp3IHPAY_cZOPmbfC7ShFc7g6VS-I",
-  authDomain: "quiz-app-70de9.firebaseapp.com",
-  projectId: "quiz-app-70de9",
-  storageBucket: "quiz-app-70de9.firebasestorage.app",
-  messagingSenderId: "674231335448",
-  appId: "1:674231335448:web:bac23f89fe03ab3547a16d"
+  apiKey: "****************",
+  authDomain: "**************",
+  projectId: "*************",
+  storageBucket: "***********",
+  messagingSenderId: "***********",
+  appId: "****************"
 };
 
 // Intialize Firebase
 const app = initializeApp(firebaseConfig)
+
 export default app;
