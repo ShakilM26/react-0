67048222c2f5fc1ffc1706c745313e386af75e79
Shakil <shakil1emperor@gmail.com>
2026-01-28 21:30:07 +0600

    Add files via upload

--- Quiz-App/firebase/auth.js
+++ Quiz-App/firebase/auth.js
@@ -1,1 +1,18 @@
+import app from "./firebase.config";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
+const auth = getAuth(app);
+
+export const signup = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const login = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+export default auth;
