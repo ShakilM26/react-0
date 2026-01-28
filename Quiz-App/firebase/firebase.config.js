bcd3ebff097f043e44b93116bced09cdfb753824
Shakil <shakil1emperor@gmail.com>
2026-01-28 22:25:20 +0600

    Add files via upload

--- Quiz-App/firebase/firebase.config.js
+++ Quiz-App/firebase/firebase.config.js
@@ -1,15 +1,14 @@
 import {initializeApp} from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "****************",
-  authDomain: "**************",
-  projectId: "*************",
-  storageBucket: "***********",
-  messagingSenderId: "***********",
-  appId: "****************"
+  apiKey: "ok",
+  authDomain: "ok",
+  projectId: "ok",
+  storageBucket: "ok",
+  messagingSenderId: "ok",
+  appId: "ok",
 };
 
 // Intialize Firebase
 const app = initializeApp(firebaseConfig)
-
 export default app;
