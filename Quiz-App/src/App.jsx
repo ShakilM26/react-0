4636dbc614ae33443654077004491bfb10a1e40e
Shakil <shakil1emperor@gmail.com>
2026-01-28 21:25:30 +0600

    Add files via upload

--- Quiz-App/src/App.jsx
+++ Quiz-App/src/App.jsx
@@ -1,1 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Playlists from "./pages/Playlists";
+import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/playlists" element={<Playlists />} />
+        <Route path="/quiz/:playlistId" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
